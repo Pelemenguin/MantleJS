9707ebcc9340ba12fcf84fa4733b3885697acd9d
Pelemenguin <elepentuin@outlook.com>
2025-07-14 20:37:52 +0800

    Add more access for Book Elements and a `fromComponent` method for Text Data

--- src/main/java/pelemenguin/mantlejs/content/book/element/BookElementInterface.java
+++ src/main/java/pelemenguin/mantlejs/content/book/element/BookElementInterface.java
@@ -1,12 +1,95 @@
 package pelemenguin.mantlejs.content.book.element;
 
-import slimeknights.mantle.client.book.data.element.TextData;
-import slimeknights.mantle.client.screen.book.element.TextElement;
+import java.util.List;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.client.gui.components.Button.OnPress;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import slimeknights.mantle.client.book.data.PageData;
+import slimeknights.mantle.client.book.data.SectionData;
+import slimeknights.mantle.client.book.data.element.ImageData;
+import slimeknights.mantle.client.book.data.element.TextComponentData;
+import slimeknights.mantle.client.book.data.element.TextData;
+import slimeknights.mantle.client.screen.book.ArrowButton;
+import slimeknights.mantle.client.screen.book.element.AnimationToggleElement;
+import slimeknights.mantle.client.screen.book.element.ArrowElement;
+import slimeknights.mantle.client.screen.book.element.ImageElement;
+import slimeknights.mantle.client.screen.book.element.ItemElement;
+import slimeknights.mantle.client.screen.book.element.ListingLeftElement;
+import slimeknights.mantle.client.screen.book.element.SelectionElement;
+import slimeknights.mantle.client.screen.book.element.SizedBookElement;
+import slimeknights.mantle.client.screen.book.element.StructureElement;
+import slimeknights.mantle.client.screen.book.element.TextComponentElement;
+import slimeknights.mantle.client.screen.book.element.TextElement;
+import slimeknights.mantle.client.screen.book.element.TooltipElement;
+import slimeknights.tconstruct.library.client.book.elements.PageIconLinkElement;
+
+@ParametersAreNonnullByDefault
 public interface BookElementInterface {
+
+    // TODO: ArrowType access
+    public static AnimationToggleElement animationToggle(int x, int y, ArrowButton.ArrowType arrowType, int arrowColor, int arrowColorHover, int arrowColorActive, StructureElement structureElement) {
+        return new AnimationToggleElement(x, y, arrowType, arrowColor, arrowColorHover, arrowColorActive, structureElement);
+    }
+
+    public static ArrowElement arrow(int x, int y, ArrowButton.ArrowType arrowType, int arrowColor, int arrowColorHover, OnPress iPressable) {
+        return new ArrowElement(x, y, arrowType, arrowColor, arrowColorHover, iPressable);
+    }
+
+    public static ImageElement image(ImageData image) {
+        return new ImageElement(image);
+    }
+    public static ImageElement image(ImageData image, int colorMultiplier) {
+        return new ImageElement(image, colorMultiplier);
+    }
+    public static ImageElement image(int x, int y, int width, int height, ImageData image) {
+        return new ImageElement(x, y, width, height, image);
+    }
+    public static ImageElement image(int x, int y, int width, int height, ImageData image, int colorMultiplier) {
+        return new ImageElement(x, y, width, height, image, colorMultiplier);
+    }
     
-    public static TextElement Text(int x, int y, int width, int height, TextData... text) {
+    public static ItemElement item(int x, int y, float scale, ItemStack... itemCycle) {
+        return new ItemElement(x, y, scale, itemCycle);
+    }
+    public static ItemElement item(int x, int y, float scale, ItemStack[] itemCycle, @Nullable String action) {
+        return new ItemElement(x, y, scale, itemCycle, action);
+    }
+
+    public static ListingLeftElement listingLeft(int x, int y, int width, int height, boolean subSection, TextData... text) {
+        return new ListingLeftElement(x, y, width, height, subSection, text);
+    }
+
+    public static PageIconLinkElement pageIconLink(int x, int y, SizedBookElement displayElement, Component name, PageData pageData) {
+        return new PageIconLinkElement(x, y, displayElement, name, pageData);
+    }
+    public static PageIconLinkElement pageIconLink(int x, int y, int w, int h, SizedBookElement displayElement, Component name, PageData pageData) {
+        return new PageIconLinkElement(x, y, w, h, displayElement, name, pageData);
+    }
+
+    public static SelectionElement selection(int x, int y, SectionData section) {
+        return new SelectionElement(x, y, section);
+    }
+
+    // TODO: StructureTemplate access
+    public static StructureElement structure(int x, int y, int width, int height, StructureTemplate template, List<StructureTemplate.StructureBlockInfo> structure) {
+        return new StructureElement(x, y, width, height, template, structure);
+    }
+
+    public static TextComponentElement textComponent(int x, int y, int width, int height, TextComponentData... text) {
+        return new TextComponentElement(x, y, width, height, text);
+    }
+
+    public static TextElement text(int x, int y, int width, int height, TextData... text) {
         return new TextElement(x, y, width, height, text);
+    }
+
+    public static TooltipElement tooltip(List<Component> tooltip, int x, int y, int width, int height) {
+        return new TooltipElement(tooltip, x, y, width, height);
     }
 
     // TODO: More Mantle Elements
--- src/main/java/pelemenguin/mantlejs/content/book/util/TextDataInterface.java
+++ src/main/java/pelemenguin/mantlejs/content/book/util/TextDataInterface.java
@@ -1,5 +1,14 @@
 package pelemenguin.mantlejs.content.book.util;
 
+import java.util.ArrayList;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.network.chat.contents.LiteralContents;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import slimeknights.mantle.client.book.data.element.TextData;
 
 public interface TextDataInterface {
@@ -8,6 +17,51 @@ public interface TextDataInterface {
 
     public static TextData literal(String string) {
         return new TextData(string);
+    }
+
+    private static String getText(MutableComponent component) {
+        String result;
+        ComponentContents contents = component.getContents();
+        if (contents instanceof LiteralContents) {
+            result = ((LiteralContents)contents).text();
+        } else if (contents instanceof TranslatableContents) {
+            result = Language.getInstance().getOrDefault((((TranslatableContents)contents).getKey()));
+        } else { // TODO: Other components
+            result = contents.toString();
+        }
+        return result;
+    }
+
+    private static TextData singleComponentToTextData(MutableComponent component) {
+        Style style = component.getStyle();
+        TextData result = literal(getText(component));
+        result.bold = style.isBold();
+        result.italic = style.isItalic();
+        result.underlined = style.isUnderlined();
+        result.strikethrough = style.isStrikethrough();
+        result.obfuscated = style.isObfuscated();
+
+        result.useOldColor = false;
+        TextColor color = style.getColor();
+        result.rgbColor = color == null ? 0 : color.getValue();
+
+        // TODO: tooltip
+
+        return result;
+    }
+
+    private static ArrayList<TextData> fromComponentToArrayList(MutableComponent component) {
+        ArrayList<TextData> result = new ArrayList<>();
+        result.add(singleComponentToTextData(component));
+        for (Component c : component.getSiblings()) {
+            result.addAll(fromComponentToArrayList(c.copy()));
+        }
+        
+        return result;
+    }
+
+    public static TextData[] fromComponent(MutableComponent component) {
+        return fromComponentToArrayList(component).toArray(new TextData[0]);
     }
 
 }
